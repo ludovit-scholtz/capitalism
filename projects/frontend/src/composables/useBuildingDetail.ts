bab47b17a0cac4ad359f7d6f9b2bab4a7a62b7a2
Ludovit Scholtz <ludovit.scholtz@aaaauto.cz>
2026-05-01 01:53:32 +0200

    refactor: remove unused lot properties from useBuildingDetail function

--- projects/frontend/src/composables/useBuildingDetail.ts
+++ projects/frontend/src/composables/useBuildingDetail.ts
@@ -4052,9 +4052,6 @@ export function useBuildingDetail() {
               cityReferenceRentPerSqm
               adjustedMarketRentPerSqm
               populationIndex
-              lotResourceTypeId
-              lotMaterialQuality
-              lotMaterialQuantity
               units {
                 id
                 buildingId
